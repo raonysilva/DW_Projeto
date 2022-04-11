9775c0b7e20d4bd792eaa28ab19b279ab27b4eff
raonysilva <79616268+raonysilva@users.noreply.github.com>
2022-04-11 11:29:30 -0300

    Add files via upload

--- ip.js
+++ ip.js
@@ -2,12 +2,13 @@ $(function() {
      $.getJSON("http://ip-api.com/json/200.129.79.33",
         function(dados) {
              $.each(dados,function(p,s){
-                 $("h3").append("<tr><td>"+p+"</td><td>"+s+"</td></tr>");
-                 const fator = ["h3"]
-                 const iptopo =fator.filter((value) = {
+               $("h3").append(p,s);
+                // $("h3").append("<tr><td>"+p+"</td><td>"+s+"</td></tr>");
+                // const fator = ["h3"]
+                // const iptopo =fator.filter((value) = {
                    
 
-                 })
+                // })
 
 
         });
