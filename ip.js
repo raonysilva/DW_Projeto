cbc112364da6108cccaaba7c83c5823918b758bf
raonysilva <79616268+raonysilva@users.noreply.github.com>
2022-04-12 01:42:14 -0300

    Add files via upload

--- ip.js
+++ ip.js
@@ -1,9 +1,13 @@
+var ip = document.getElementById("ip");
+
+
 $(function() {
-     $.getJSON("http://ip-api.com/json/200.129.79.33",
+     $.getJSON("http://ip-api.com/json/?fields=message,continent,country,region,regionName,city,isp,org,query",
         function(dados) {
-             $.each(dados,function(p,s){
-               $("h3").append(p,s);
-                // $("h3").append("<tr><td>"+p+"</td><td>"+s+"</td></tr>");
+             $.each(dados,function(p,s,){
+               
+               $("h3").append("<td> " +s+ " </td>");
+                //$("h4").append("<tr><td>"+p+"</td><td>"+s+"</td></tr>");
                 // const fator = ["h3"]
                 // const iptopo =fator.filter((value) = {
                    
@@ -11,9 +15,26 @@ $(function() {
                 // })
 
 
-        });
+          });
 
      });
             
 
 })
+
+document.getElementById("submit").addEventListener("click", buscarip);
+function buscarip(){
+     $.getJSON("http://ip-api.com/json/{query}?fields=message,continent,country,region,regionName,city,isp,org,query",
+          function(dados2){
+               $.each(dados2,function(r,a){
+                $("h4").append("<tr><td>"+r+"</td><td>"+a+"</td></tr>");
+          });
+
+     });
+
+     
+
+}
+
+
+//$.getJSON("http://ip-api.com/json/?fields=status,message,continent,country,region,regionName,city,isp,org,query",
