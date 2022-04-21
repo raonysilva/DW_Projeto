6c1f12d594fdddaf9bfdec2df94f6a9f195c5bb3
raonysilva <79616268+raonysilva@users.noreply.github.com>
2022-04-21 13:48:44 -0300

    Add files via upload

--- ip.js
+++ ip.js
@@ -4,9 +4,7 @@ $(function() {
         function(dados) {
              $.each(dados,function(p,s,){
                
-               $("h3").append("<td>&nbsp" +s+ " &nbsp</td>");
-               //$("h4").append(ips.value);
-               
+               $("h3").append("<td>&nbsp" +s+ " &nbsp</td>");           
 
 
           });
@@ -19,23 +17,46 @@ $(function() {
 
 
 function buscarip() {
+     
      $("#buscar").click(function(){
           $('#saida').html("");
      });
-
+     
      $.getJSON("http://ip-api.com/json/"+document.getElementById("nome").value+"?lang=pt-BR&fields=message,continent,country,region,regionName,city,isp,org,lat,lon,query",
         function(dados2) {
              $.each(dados2,function(r,a,){
                
                $(document.getElementById("saida")).append("<tr><td>"+r+"</td><td>"+a+"</td></tr>");
+
                
-
-
           });
 
      });
+     
+     $.getJSON("http://ip-api.com/json/"+document.getElementById("nome").value+"?lang=pt-BR&fields=lat",
+        function(dados3) {
+          
+          $.each(dados3,function(j,f){
+               lat = f;
+                    
 
-     //document.getElementById("saida").value= " ";
+          });
+                        
+
+     });
+
+     $.getJSON("http://ip-api.com/json/"+document.getElementById("nome").value+"?lang=pt-BR&fields=lon",
+        function(dados4) {
+          $.each(dados4,function(p,q){
+               long = q; 
+               
+          });  
+                 
+         
+
+     });
+
+     document.getElementsByName("mapa").src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15833.207449695528!2d-34.85136865!3d-7.2063619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1650545403294!5m2!1spt-BR!2sbr";  
      
 
 }
@@ -58,6 +79,3 @@ container.addEventListener('click', (event) => {
 	closeModal();
 });
 
-
-
-//$.getJSON("http://ip-api.com/json/?fields=status,message,continent,country,region,regionName,city,isp,org,query",
