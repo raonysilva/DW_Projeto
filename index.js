3ce751a81edc041fd19a1aa49139e649c40dc4c2
raonysilva <79616268+raonysilva@users.noreply.github.com>
2022-05-17 23:35:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 const express = require('express');
 const app=express();
 const porta=process.env.port;
-const bodyParser = require('body-parser');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
+//Json
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.joson");
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(router);
 
+//HTML
 app.get('/',(req,res)=>{
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 })
 app.get('/cadastro.html', function(req, res){
-    res.sendfile(__dirname + '/cadastro.html');
+    res.sendFile(__dirname + '/cadastro.html');
 });
 app.get('/ip.html', function(req, res){
-    res.sendfile(__dirname + '/ip.html');
+    res.sendFile(__dirname + '/ip.html');
 });
 
-
-
+//CSS
 app.get('/css/index.css', function(req, res){
-    res.sendfile(__dirname + '/css/index.css');
+    res.sendFile(__dirname + '/css/index.css');
 });
 app.get('/css/cadastro.css', function(req, res){
-    res.sendfile(__dirname + '/css/cadastro.css');
+    res.sendFile(__dirname + '/css/cadastro.css');
 });
 app.get('/css/ip.css', function(req, res){
-    res.sendfile(__dirname + '/css/ip.css');
+    res.sendFile(__dirname + '/css/ip.css');
 });
 
-
+//IMGAGEM
 app.get('/img/user4.png', function(req, res){
-    res.sendfile(__dirname + '/img/user4.png');
+    res.sendFile(__dirname + '/img/user4.png');
 });
 app.get('/img/mundo.png', function(req, res){
-    res.sendfile(__dirname + '/img/mundo.png');
+    res.sendFile(__dirname + '/img/mundo.png');
 });
 
-
+//JS
 app.get('/js/init.js', function(req, res){
-    res.sendfile(__dirname + '/js/init.js');
+    res.sendFile(__dirname + '/js/init.js');
 });
 app.get('/js/ip.js', function(req, res){
-    res.sendfile(__dirname + '/js/ip.js');
+    res.sendFile(__dirname + '/js/ip.js');
 });
 app.get('/js/cadastro.js', function(req, res){
-    res.sendfile(__dirname + '/js/cadastro.js');
+    res.sendFile(__dirname + '/js/cadastro.js');
 });
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.post('/',(req,res)=>{
-    console.log(req.body.login);
-    res.sendfile(__dirname + '/index.html');
-
-})
 
 app.listen(porta || 3000,()=>{console.log('Servidor Funcionando')});
